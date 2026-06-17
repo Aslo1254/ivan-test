f5ca6c0adc80f5d13c8601d54155f5051015ea0d
Aslo1254 <79385506+Aslo1254@users.noreply.github.com>
2026-06-17 14:13:56 +0000

    Add files via upload

--- api/generate.js
+++ api/generate.js
@@ -38,9 +38,7 @@ function buildEchangeCharges(params){
   template.forEach(function(ch){
     const code = resolveEchCode(ch.code, io);
     // Commodity-driven charges: Redevance Portuaire / Municipale / Communale (base TO with commodity rate)
-    // NB San Pedro : "Communaute portuaire" n'est PAS une redevance portuaire marchandise -> on l'exclut
-    // pour qu'elle reste une charge standard au poids (qty = poids total, taux du template), pas au taux marchandise.
-    const isPort = /portuaire|red port(?! transit)/i.test(ch.d) && ch.base==='TO' && !/communaut/i.test(ch.d);
+    const isPort = /portuaire|red port(?! transit)/i.test(ch.d) && ch.base==='TO';
     const isMun = /municipale|red mun/i.test(ch.d);
     const isCom = /communale|red com/i.test(ch.d);
 
